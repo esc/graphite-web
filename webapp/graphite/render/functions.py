fix off-by-one error

webapp/graphite/render/functions.py
@@ -3075,7 +3075,7 @@ def sixSigma(requestContext,
     it_std = it.std(axis=0)
 
     # figure out how many bins to keep
-    to_keep = (end-start) / shifted.step
+    to_keep = ((end-start) / shifted.step ) + 1
     # keep only the relevant bins
     it_mean = it_mean[-to_keep:]
     it_std = it_std[-to_keep:]