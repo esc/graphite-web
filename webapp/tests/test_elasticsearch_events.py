e17ad601fb52bbcac81bfb2a6fad077c26c3d882
Valentin Haenel <valentin.haenel@gmx.de>
2014-03-21 12:27:48 +0100

    refactor tests to test function with less scope

--- webapp/tests/test_elasticsearch_events.py
+++ webapp/tests/test_elasticsearch_events.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.test import TestCase
 from graphite.elasticsearchevents.models import Event
 
@@ -8,40 +6,24 @@ class TestEventBuildQuery(TestCase):
 
     @staticmethod
     def _expected(tag_string=''):
-        expected = {'filter':
-                    {'and':
-                     [{'query': {'query_string': {'query': '%s' % tag_string}}},
-                      {'range': {'@timestamp': {'gte': '21600000', 'lte': '108000000'}}}
-                      ]},
-                    'sort': {'@timestamp': {'order': 'desc'}},
-                    'size': 500
-                    }
-        return expected
+        return [{'query': {'query_string': {'query': '%s' % tag_string}}}]
 
     def test_build_query_empty_string(self):
         query = Event._build_tag_query_string('')
-        self.assertEqual(query, [{'query': {'query_string': {'query': ''}}}])
+        self.assertEqual(query, self._expected())
 
     def test_build_query_single_tag(self):
-        query = Event.buildQuery(['tag1'],
-                                 time_from=datetime(1970, 1, 1),
-                                 time_until=datetime(1970, 1, 2))
+        query = Event._build_tag_query_string(['tag1'])
         self.assertEqual(query, self._expected('tags:tag1'))
 
     def test_build_query_double_tag(self):
-        query = Event.buildQuery(['tag1', 'tag2'],
-                                 time_from=datetime(1970, 1, 1),
-                                 time_until=datetime(1970, 1, 2))
+        query = Event._build_tag_query_string(['tag1', 'tag2'])
         self.assertEqual(query, self._expected('tags:tag1 AND tags:tag2'))
 
     def test_build_query_single_host_tag(self):
-        query = Event.buildQuery(['HOST:devfoo01'],
-                                 time_from=datetime(1970, 1, 1),
-                                 time_until=datetime(1970, 1, 2))
+        query = Event._build_tag_query_string(['HOST:devfoo01'])
         self.assertEqual(query, self._expected('HOST:devfoo01'))
 
     def test_build_query_double_host_tag(self):
-        query = Event.buildQuery(['HOST:devfoo01', 'HOST:devfoo02'],
-                                 time_from=datetime(1970, 1, 1),
-                                 time_until=datetime(1970, 1, 2))
+        query = Event._build_tag_query_string(['HOST:devfoo01', 'HOST:devfoo02'])
         self.assertEqual(query, self._expected('HOST:devfoo01 AND HOST:devfoo02'))
