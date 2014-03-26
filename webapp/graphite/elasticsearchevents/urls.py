41ec7555ac3dba597ea5c88c9858dd890038b1d8
Valentin Haenel <valentin.haenel@gmx.de>
2014-03-26 13:29:38 +0100

    fix url pattern, elasticsearch might also use -

--- webapp/graphite/elasticsearchevents/urls.py
+++ webapp/graphite/elasticsearchevents/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns('graphite.elasticsearchevents.views',
   ('^get_data?$', 'get_data'),
-  (r'(?P<event_id>\w+)/$', 'detail'),
+  (r'(?P<event_id>[^/]+)/$', 'detail'),
   ('^$', 'view_events'),
 )
